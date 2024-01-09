c7bd49c0e980bb638573694cc9ea6ad8864bc2b7
Qotayba Mohammad <46694587+ktsyr1@users.noreply.github.com>
2024-01-09 23:43:05 +0200

    add

--- src/theme/footer.jsx
+++ src/theme/footer.jsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+import Script from "next/script";
 
 
 export function Footer() {
@@ -10,38 +12,38 @@ export function Footer() {
                             <div className="col-xl-4 col-lg-4 col-md-6">
                                 <div className="footer-widget">
                                     <div className="logo mb-30">
-                                        <a href="/">
+                                        <Link href="/">
                                             <img src="/images/logo2.png" alt="" style={{ borderRadius: '30px' }} />
-                                        </a>
+                                        </Link>
                                     </div>
                                     <p className="  mb-30 text-white">
                                         شركة تقوم في خدمة العملاء في تنفيذ التصميم الإعلاني والديكور وبرمجة التطبيقات ومواقع الويب.
                                     </p>
                                     <ul className="socials">
                                         <li>
-                                            <a href="https://t.me/octo_tech">
+                                            <Link href="https://t.me/octo_tech">
                                                 <i className="lni lni-telegram"></i>
-                                            </a>
+                                            </Link>
                                         </li>
                                         <li>
-                                            <a href="https://www.facebook.com/octopuss.tech">
+                                            <Link href="https://www.facebook.com/octopuss.tech">
                                                 <i className="lni lni-facebook-filled"></i>
-                                            </a>
+                                            </Link>
                                         </li>
                                         <li>
-                                            <a href="https://sy.linkedin.com/company/octop-tech">
+                                            <Link href="https://sy.linkedin.com/company/octop-tech">
                                                 <i className="lni lni-linkedin-original"></i>
-                                            </a>
+                                            </Link>
                                         </li>
                                         <li>
-                                            <a href="https://www.instagram.com/octopu.tech/">
+                                            <Link href="https://www.instagram.com/octopu.tech/">
                                                 <i className="lni lni-instagram-filled"></i>
-                                            </a>
+                                            </Link>
                                         </li>
                                         <li>
-                                            <a href="https://www.youtube.com/@octo-tech">
+                                            <Link href="https://www.youtube.com/@octo-tech">
                                                 <i className="lni lni-youtube"></i>
-                                            </a>
+                                            </Link>
                                         </li>
                                         <div></div>
                                     </ul>
@@ -52,9 +54,9 @@ export function Footer() {
                                 <div className="footer-widget">
                                     <h3>حول الشركة</h3>
                                     <ul className="links">
-                                        <li><a href="/">الرئيسية</a></li>
-                                        <li><a href="/#service">الخدمات</a></li>
-                                        <li><a href="/blog">المدونة</a></li>
+                                        <li><Link href="/">الرئيسية</Link></li>
+                                        <li><Link href="/#service">الخدمات</Link></li>
+                                        <li><Link href="/blog">المدونة</Link></li>
                                     </ul>
                                 </div>
                             </div>
@@ -63,26 +65,25 @@ export function Footer() {
                                 <div className="footer-widget">
                                     <h3>أخر التدوينات</h3>
                                     <ul className="links">
-                                        <li><a href="/#">How it works</a></li>
-                                        <li><a href="/#">Privacy policy</a></li>
-                                        <li><a href="/#">Terms of service</a></li>
-                                        <li><a href="/#">Refund policy</a></li>
+                                        <li><Link href="/#">How it works</Link></li>
+                                        <li><Link href="/#">Privacy policy</Link></li>
+                                        <li><Link href="/#">Terms of service</Link></li>
+                                        <li><Link href="/#">Refund policy</Link></li>
                                     </ul>
                                 </div>
                             </div>
 
-                         
+
                         </div>
                     </div>
                 </div>
 
-                <a href="#" className="scroll-top btn-hover">
+                <Link href="#" className="scroll-top btn-hover">
                     <i className="lni lni-chevron-up"></i>
-                </a>
+                </Link>
                 {/* <!-- ======== JS here ======== --> */}
-                <script src="assets/js/bootstrap.bundle.min.js"></script>
-                <script src="assets/js/wow.min.js"></script>
-                {/* <script src="assets/js/main.js"></script>  */}
+                <Script src="assets/js/bootstrap.bundle.min.js"></Script>
+                <Script src="assets/js/wow.min.js"></Script>
             </footer>
         </>
     )
