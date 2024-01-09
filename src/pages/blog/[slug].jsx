4578f37ec86f3743d22ed5b7418772e1770da3f7
Qotayba Mohammad <46694587+ktsyr1@users.noreply.github.com>
2024-01-09 23:12:29 +0200

    Update [slug].jsx

--- src/pages/blog/[slug].jsx
+++ src/pages/blog/[slug].jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 import MarkdownIt from 'markdown-it'
 
-let api = `${process.env.NEXT_PUBLIC_API}/client/posts`
 export async function getStaticPaths() {
+    let api = `${process.env.NEXT_PUBLIC_API}/client/posts`
     let { data } = await axios.get(`${api}`);
     let paths = data.map(App => ({ params: { slug: App.url } }))
     return { paths, fallback: true }
@@ -10,6 +10,7 @@ export async function getStaticPaths() {
 
 // get static props
 export async function getStaticProps(ctx) {
+    let api = `${process.env.NEXT_PUBLIC_API}/client/posts`
     let { data } = await axios.get(`${api}/${encodeURI(ctx.params.slug)}`);
     return { props: { data }, revalidate: 10 * 60 }
 }
@@ -25,10 +26,10 @@ export default function PageOne({ data }) {
             <img src={data?.image} alt={data?.title} style={{ borderRadius: '20px' }} />
 
             {/* content */}
-            <h1 className="py-10">{data?.title}</h1>
-            <p className="p-10 my-10" style={{ backgroundColor: '#eee', borderRadius: '10px' }}>{data?.bio}</p>
+            <h1 className="py-10">{data.title}</h1>
+            <p className="p-10 my-10" style={{ backgroundColor: '#eee', borderRadius: '10px' }}>{data.bio}</p>
             {/* view md  */}
-            <div dangerouslySetInnerHTML={{ __html: md.render(data?.content) }} />
+            <div dangerouslySetInnerHTML={{ __html: md.render(data.content) }} />
         </div>
     )
 }
