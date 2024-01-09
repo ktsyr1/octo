5c53ea6ecea4cb22e4bc5f83773e1b10655b8293
Qotayba Mohammad <46694587+ktsyr1@users.noreply.github.com>
2024-01-09 11:07:20 +0200

    add

--- src/pages/blog/[slug].jsx
+++ src/pages/blog/[slug].jsx
@@ -1,19 +1,18 @@
 import axios from "axios";
 
-export async function getStaticPaths() {
-    let { data } = await axios.get(`${process.env.NEXT_PUBLIC_API}/client/posts`);
-    let paths = data.map(App => ({ params: { slug: App.url } }))
-    return { paths, fallback: true }
-}
+// export async function getStaticPaths() {
+//     let { data } = await axios.get(`${process.env.NEXT_PUBLIC_API}/client/posts`);
+//     let paths = data.map(App => ({ params: { slug: App.url } }))
+//     return { paths, fallback: true }
+// }
 
-// get static props
-export async function getStaticProps(ctx) {
-    let { data } = await axios.get(`${process.env.NEXT_PUBLIC_API}/client/posts/${encodeURI(ctx.params.slug)}`);
-    return { props: { data }, revalidate: 10 * 60 }
-}
+// // get static props
+// export async function getStaticProps(ctx) {
+//     let { data } = await axios.get(`${process.env.NEXT_PUBLIC_API}/client/posts/${encodeURI(ctx.params.slug)}`);
+//     return { props: { data }, revalidate: 10 * 60 }
+// }
 
 export default function PageOne({ data }) {
-    console.log(data);
     return (
         <article className="box col aitem" >
             {/* content */}
