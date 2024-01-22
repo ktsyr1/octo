61bc70cf803aa36e86e00787386da4ad30cd9798
Qotayba Mohammad <46694587+ktsyr1@users.noreply.github.com>
2024-01-22 22:00:15 +0200

    add

---
+++ src/pages/blog/index.jsx
@@ -0,0 +1,11 @@
+import axios from 'axios';
+import Link from 'next/link';
+import { Landing } from '../theme/Landing';
+
+export const getStaticProps = async ({ query }) => {
+    let { data } = await axios.get(`${process.env.NEXT_PUBLIC_API}/client/posts`);
+    return { props: { data } }
+}
+export default function Home({ data }) {
+    return <Landing data={data} />
+} 
