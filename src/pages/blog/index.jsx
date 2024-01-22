3d445ddf0647c3090091e7075170fed81e5923b9
Qotayba Mohammad <46694587+ktsyr1@users.noreply.github.com>
2024-01-22 22:00:28 +0200

    test

--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -1,11 +1,14 @@
 import axios from 'axios';
-import Link from 'next/link';
-import { Landing } from '../theme/Landing';
+import Link from 'next/link'; 
 
 export const getStaticProps = async ({ query }) => {
     let { data } = await axios.get(`${process.env.NEXT_PUBLIC_API}/client/posts`);
     return { props: { data } }
 }
 export default function Home({ data }) {
-    return <Landing data={data} />
+    return  (
+        <div >
+            <h1>b</h1>
+        </div>
+    )
 } 
