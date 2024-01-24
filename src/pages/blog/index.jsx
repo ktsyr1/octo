045dfde9b24bb80fe51cda2d6c326c2ba56fa620
Qotayba Mohammad <46694587+ktsyr1@users.noreply.github.com>
2024-01-24 10:28:22 +0200

    UpdateCommitAuto

--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -1,14 +1,20 @@
+import Card from '@/theme/card';
 import axios from 'axios';
-import Link from 'next/link'; 
+import Link from 'next/link';
 
 export const getStaticProps = async ({ query }) => {
     let { data } = await axios.get(`${process.env.NEXT_PUBLIC_API}/client/posts`);
     return { props: { data } }
 }
 export default function Home({ data }) {
-    return  (
-        <div >
-            <h1>b</h1>
+    return (
+        <div className='box col j aitem' >
+            <h1>المقالات</h1>
+            <p> افضل المقالات المختارة بعناية المخصصة لك من كل العلوم برمجة وتصميم و الاخبار التقنية  والكثير لك </p>
+            <br />
+            <div className="box grid j m-20">
+                {data.map(post => <Card data={post} />)}
+            </div>
         </div>
     )
 } 
