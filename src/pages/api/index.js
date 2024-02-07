486311580b5a2a333ba3b378f82e207f9d8da868
Qotayba Mohammad <46694587+ktsyr1@users.noreply.github.com>
2024-02-07 22:13:05 +0200

    UpdateCommitAuto

--- src/pages/api/index.js
+++ src/pages/api/index.js
@@ -1,26 +1,27 @@
 import { Posts } from "@/models"
 import API from "nextjs-vip"
- 
+
 export default async function SiteMap(req, res, next) {
 
     let app = new API(req, res)
     app.get(async () => {
 
         let page = await Posts.find().select("create_at url")
-        let data = []
         let { domain } = req.query
+        let data = [
+            { url: `${domain}/` }
+        ]
 
         function DATE(e) {
             if (e) {
                 return new Date(e)?.toISOString().split('T')[0]
             } else return
         }
-        page
-            .map(a => data.push({
-                url: `${domain}/blog/${a.url}`,
-                date: DATE(a?.create_at)
-            })
-            )
+        page.map(a => data.push({
+            url: `${domain}/blog/${a.url}`,
+            lastmod: DATE(a?.create_at)
+        })
+        )
         app.Send(data)
     }
     )
